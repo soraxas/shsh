bacd17686dbf7368af22a4d154022c2d0abc4b38
Tin Lai <tin@tinyiu.com>
2025-04-22 10:00:42 +1000

    use info_echo in correct places
    Signed-off-by: Tin Lai <tin@tinyiu.com>

--- bootstrap/install.sh
+++ bootstrap/install.sh
@@ -7,27 +7,27 @@ eval "$(curl -s https://raw.githubusercontent.com/soraxas/shsh/master/libexec/sh
 
 ## stop if basher is already installed
 if [ -d "$SHSH_ROOT" ]; then
-  echo "shsh already exists at '$SHSH_ROOT'." >&2
-  echo "Delete it if you want to start this bootstrap process again." >&2
+  error_echo "shsh already exists at '$SHSH_ROOT'."
+  error_echo "Delete it if you want to start this bootstrap process again."
   exit 1
 fi
 
 
-echo "Installing shsh to '$SHSH_ROOT'"
+info_echo "Installing shsh to '$SHSH_ROOT'"
 git clone https://github.com/soraxas/shsh.git "$SHSH_ROOT" > /dev/null
 
 # Check shell type
 shell_type=$(basename "$SHELL")
-echo "Detected shell type: $shell_type"
+info_echo "Detected shell type: $shell_type"
 case "$shell_type" in
   bash)  startup_type="simple" ; startup_script="$HOME/.bashrc" ;;
   zsh)   startup_type="simple" ; startup_script="$HOME/.zshrc"  ;;
   sh)    startup_type="simple" ; startup_script="$HOME/.profile";;
   fish)  startup_type="fish"   ; startup_script="$HOME/.config/fish/config.fish"  ;;
   *)
-    echo "Unknown shell '$shell_type'." >&2
-    echo "Perhaps set the \$SHELL environment variable to your shell type before running this?" >&2
-    echo "e.g. export SHELL=bash" >&2
+    error_echo "Unknown shell '$shell_type'."
+    error_echo "Perhaps set the \$SHELL environment variable to your shell type before running this?"
+    error_echo "e.g. export SHELL=bash"
     exit 1
     ;;
 esac
@@ -39,7 +39,7 @@ if
 
 
 ## now add the basher initialisation lines to the user's startup script
-echo "Adding shsh initialisation"
+info_echo "Adding shsh initialisation"
 #shellcheck disable=SC2016
 case "$startup_type" in
   simple)
@@ -55,9 +55,9 @@ case "$startup_type" in
     printf '%s\n' 'status --is-interactive; and shsh init fish | source' >>"$startup_script"
     ;;
   *)
-    echo "Unknown shell '$shell_type'." >&2
-    echo "Perhaps set the \$SHELL environment variable to your shell type before running this?" >&2
-    echo "e.g. export SHELL=bash" >&2
+    error_echo "Unknown shell '$shell_type'."
+    error_echo "Perhaps set the \$SHELL environment variable to your shell type before running this?"
+    error_echo "e.g. export SHELL=bash"
     exit 1
     ;;
 esac
